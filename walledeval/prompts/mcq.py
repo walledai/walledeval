1ad004d91926f36dde806e4b750dfdb7ee0c40f4
Prannaya <prannayagupta@gmail.com>
2024-05-23 19:44:36 +0800

    feat(template): implement functional mcq templates

--- walledeval/prompts/mcq.py
+++ walledeval/prompts/mcq.py
@@ -1,31 +1,34 @@
 # walledeval/prompts/mcq.py
 
+from walledeval.constants import DEFAULT_OPTIONS, DEFAULT_SAMPLE_QUESTION, DEFAULT_OPTION_FORMAT
 from walledeval.prompts.core import PromptTemplate
 from walledeval.types import MultipleChoiceQuestion
 
 __all__ = [
     "MultipleChoiceTemplate",
     "FewShotMCQTemplate",
-    "DEFAULT_OPTIONS", "DEFAULT_SAMPLE_QUESTION"
     
 ]
 
-DEFAULT_OPTIONS = [chr(idx) for idx in range(65, 91)]
-
-DEFAULT_SAMPLE_QUESTION = MultipleChoiceQuestion(
-    question = "Which of the following is a fruit?",
-    choices = ["Spider", "Apple", "Lamp", "Cloud"],
-    answer = 1
-)
-
 _INSPECT_TEMPLATE = """
-Answer the following multiple choice question. The entire content of your response should be of the following format: 'ANSWER: $LETTER' (without quotes) where LETTER is one of {letters}.
+Answer the following multiple choice question. The entire content of your response should be of the following format: 'ANSWER: $$LETTER' (without quotes) where LETTER is one of $options.
 
 $question
 
 $choices
 
 ANSWER:""".strip()+" "
+
+_DEFAULT_TEMPLATE = """
+Answer the following multiple choice question. The entire content of your response should be confined to the option. Choose from $options.
+
+$question
+
+$choices
+
+Answer:""".strip()+" "
+
+
 
 class MultipleChoiceTemplate(PromptTemplate):
     """Multiple Choice Prompt Template.
@@ -39,11 +42,11 @@ class MultipleChoiceTemplate(PromptTemplate):
     def __init__(self, 
                  template: str, 
                  options: list[str] = DEFAULT_OPTIONS,
-                 option_format: str = "{}. "):
+                 option_format: str = DEFAULT_OPTION_FORMAT):
         """Inits MultipleChoiceTemplate.
 
         Args:
-            template (str): A string with $-delimited substitutions. MUST contain $question and $choices.
+            template (str): A string with $-delimited substitutions. MUST contain $question, $options and $choices.
             options (list[str], optional): MCQ options to input to model. Defaults to ["A", "B", "C", "D", ...].
             option_format (str, optional): Format string for these options. Defaults to "{}. ".
         """
@@ -52,11 +55,31 @@ class MultipleChoiceTemplate(PromptTemplate):
         
         self.options = options
         self.option_format = option_format
-    
+
     @classmethod
-    def inspect_method(cls, 
-                       options: list[str] = DEFAULT_OPTIONS,
-                       option_format: str = "{}. "):
+    def default(cls, 
+                options: list[str] = DEFAULT_OPTIONS,
+                option_format: str = DEFAULT_OPTION_FORMAT):
+        """Default Template for Multiple Choice.
+        
+        Args:
+            options (list[str], optional): MCQ options to input to model. Defaults to ["A", "B", "C", "D", ...].
+            option_format (str, optional): Format string for these options. Defaults to "{}. ".
+
+        Returns:
+            _type_: A MultipleChoiceTemplate object
+        """
+        return cls(
+            _DEFAULT_TEMPLATE,
+            options = options,
+            option_format = option_format
+        )
+
+
+    @classmethod
+    def inspect(cls,
+                options: list[str] = DEFAULT_OPTIONS,
+                option_format: str = DEFAULT_OPTION_FORMAT):
         """Inspect Template for Multiple Choice.
         
         Sourced from [here](https://github.com/UKGovernmentBEIS/inspect_ai/blob/main/src/inspect_ai/solver/_multiple_choice.py#L18).
@@ -94,8 +117,23 @@ class MultipleChoiceTemplate(PromptTemplate):
         return super().format(
             question = question.question,
             choices = choices_str,
+            options = self.options[:len(question.choices)],
             **kwds
         )
+        
+        
+_DEFAULT_QUESTION_TEMPLATE = """
+$question
+
+$choices
+
+Choose from $options.
+
+Answer:""".strip()+" "
+
+_DEFAULT_PRECEDING_INSTRUCTION = """
+Answer the following multiple choice question. The entire content of your response should be confined to the options indicated.
+"""
 
 
 
@@ -103,7 +141,7 @@ class FewShotMCQTemplate(MultipleChoiceTemplate):
     """Few-Shot Multiple Choice Prompting Template.
 
     Attributes:
-        template (str): A string with $-delimited substitutions. MUST contain $question and $choices.
+        template (str): A string with $-delimited substitutions. MUST contain $question, $options and $choices.
         options (list[str]): MCQ options to input to model.
         option_format (str): Format string for these options.
         sample_questions: (list[MultipleChoiceQuestion]) for 
@@ -112,9 +150,10 @@ class FewShotMCQTemplate(MultipleChoiceTemplate):
     def __init__(self,
                  template: str,
                  options: list[str] = DEFAULT_OPTIONS,
-                 option_format: str = "{}.",
+                 option_format: str = DEFAULT_OPTION_FORMAT,
                  samples: list[MultipleChoiceQuestion] = [DEFAULT_SAMPLE_QUESTION],
                  preceding_instruction: str = "",
+                 boxed_answer: bool = False,
                  **sample_kwds):
         """Inits FewShotMCQTemplate.
 
@@ -124,24 +163,43 @@ class FewShotMCQTemplate(MultipleChoiceTemplate):
             option_format (str, optional): Format string for these options. Defaults to "{}. ".
             samples (list[MultipleChoiceQuestion], optional): List of sample questions. Defaults to [ MultipleChoiceQuestion( question = "Which of the following is a fruit?", choices = ["Spider", "Apple", "Lamp", "Cloud"], answer = 1 ) ].
             preceding_instruction (str, optional): Preceding Instruction, if need be. Defaults to "".
+            boxed_answer (bool, optional): Whether or not to box up the sample answers.
         """
         
         super().__init__(template, options, option_format)
         
         self.sample_questions = samples
         
+        self.boxed_answer = boxed_answer
+        
         self.sample = ("\n\n".join([preceding_instruction] + [
-            super().format(
-                question = sample.question,
-                choices = sample.choices,
+            MultipleChoiceTemplate.format(
+                self,
+                question = sample,
                 **sample_kwds
-            ) + self.options[sample.answer]
+            ) + (
+                "\\boxed{"+self.options[sample.answer]+"}"
+                if boxed_answer else 
+                self.options[sample.answer] 
+            )
             for sample in samples
         ])).lstrip()
+        
+    @classmethod
+    def default(cls,
+                options: list[str] = DEFAULT_OPTIONS,
+                option_format: str = DEFAULT_OPTION_FORMAT,
+                samples: list[MultipleChoiceQuestion] = [DEFAULT_SAMPLE_QUESTION],
+                boxed_answer: bool = False):
+        return cls(
+            _DEFAULT_QUESTION_TEMPLATE,
+            options, option_format, samples,
+            _DEFAULT_PRECEDING_INSTRUCTION,
+            boxed_answer
+        )
     
     def format(self,
-               question: str,
-               choices: list[str],
+               question: MultipleChoiceQuestion,
                **kwds) -> str:
         """Format Few-Shot Multiple Choice Template with given expected inputs and keywords.
 
@@ -153,5 +211,6 @@ class FewShotMCQTemplate(MultipleChoiceTemplate):
         """
         return (
             self.sample + "\n\n" + 
-            super().format(question, choices, **kwds)
+            super().format(question, **kwds) +
+            "\\boxed{" if self.boxed_answer else ""
         )
