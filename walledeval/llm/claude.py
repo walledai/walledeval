fe2c0905a6d5329ad531da39cab0263e60692b54
Prannaya Gupta <50899509+ThePyProgrammer@users.noreply.github.com>
2024-05-24 03:04:42 +0000

    feat(claude): implement sonnet and haiku + linting changes

--- walledeval/llm/claude.py
+++ walledeval/llm/claude.py
@@ -10,22 +10,43 @@ __all__ = [
 
 
 class Claude(LLM):
-    def __init__(self, api_key: str, system_prompt: str = ""):
-        super().__init__("Claude 3 Opus", system_prompt)
+    def __init__(self, model_id: str, api_key: str, system_prompt: str = ""):
+        super().__init__(model_id, system_prompt)
         self.client = Anthropic(api_key=api_key)
-    
-    def generate(self, text: str, 
-                 max_tokens: int = 1024,
+
+    @classmethod
+    def opus(cls, api_key: str, system_prompt: str = ""):
+        return cls(
+            "claude-3-opus-20240229",
+            api_key, system_prompt
+        )
+
+    @classmethod
+    def sonnet(cls, api_key: str, system_prompt: str = ""):
+        return cls(
+            "claude-3-sonnet-20240229",
+            api_key, system_prompt
+        )
+
+    @classmethod
+    def haiku(cls, api_key: str, system_prompt: str = ""):
+        return cls(
+            "claude-3-haiku-20240307",
+            api_key, system_prompt
+        )
+
+    def generate(self, text: str,
+                 max_new_tokens: int = 1024,
                  temperature: float = 0) -> str:
         message = self.client.messages.create(
-            max_tokens=max_tokens,
+            max_tokens=max_new_tokens,
             messages=[{
                 "role": "user",
                 "content": text
             }],
             temperature=temperature,
             system=self.system_prompt,
-            model="claude-3-opus-20240229",
+            model=self.name
         )
         output = message.content[0].text
         return output
