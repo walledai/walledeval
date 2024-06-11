b942df5798ff02d91101e72fddab459b7d8ec95b
Prannaya <prannayagupta@gmail.com>
2024-06-11 13:23:40 +0800

    feat(llmasajudge): implement LLMasaJudge with system prompt ability

---
+++ walledeval/judge/llm/system.py
@@ -0,0 +1,29 @@
+# walledeval/judge/llm/system.py
+
+from abc import ABC, abstractmethod
+from typing import Generic, TypeVar
+
+from walledeval.judge.llm.core import LLMasaJudge
+
+__all__ = ["SystemLLMasaJudge"]
+
+
+O = TypeVar("O") # Output Field
+
+
+class SystemLLMasaJudge(LLMasaJudge[O], Generic[O], ABC):
+    def generate(self, response: str, system: str) -> str:
+        return self._llm.generate([
+            {
+                "role": "system",
+                "content": system
+            },
+            {
+                "role": "system",
+                "content": response
+            }
+        ], instruct=True)
+
+    @abstractmethod
+    def process_llm_output(self, response: str) -> O:
+        pass
