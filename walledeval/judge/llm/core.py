b1950a1701d20f9a4d35b85191eb2601ead5ae3a
Prannaya <prannayagupta@gmail.com>
2024-06-11 13:21:09 +0800

    feat(llmasajudge): implement abstract `LLMasaJudge` class

---
+++ walledeval/judge/llm/core.py
@@ -0,0 +1,42 @@
+# walledeval/judge/llm/core.py
+
+from abc import ABC, abstractmethod
+from typing import Generic, TypeVar
+
+from walledeval.llm import LLM
+from walledeval.judge.core import Judge
+
+__all__ = [
+    "LLMasaJudge"
+]
+
+O = TypeVar('O') # Output Field
+
+
+class LLMasaJudge(Judge[None, O], ABC, Generic[O]):
+    def __init__(self,
+                 name: str,
+                 llm: LLM):
+        super().__init__(name)
+        self._llm = llm
+    
+    def set_system_prompt(self,
+                          system_prompt: str):
+        self._llm = self._llm.set_system_prompt(system_prompt)
+        
+    def generate(self, response: str) -> str:
+        return self._llm.generate(
+            response,
+            instruct=True
+        )
+
+    @abstractmethod
+    def process_llm_output(self, response: str) -> O:
+        pass
+
+    def check(self, response: str, answer: None = None) -> O:
+        llm_output = self.generate(response)
+
+        output = self.process_llm_output(llm_output)
+
+        return output
