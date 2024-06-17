fc4c011854534e2e5e5e7db70b3f6bf66abae98f
Prannaya <prannayagupta@gmail.com>
2024-06-17 16:24:31 +0800

    fix(range): fix `process_range` button

--- walledeval/util/__init__.py
+++ walledeval/util/__init__.py
@@ -7,31 +7,31 @@ __all__ = [
 ]
 
 
-def process_range(range: Range, length: int) -> list[int]:
+def process_range(range_obj: Range, length: int) -> list[int]:
     idx_list: list[int]
 
-    if isinstance(range, int):
-        if range < -length:
-            raise IndexError(f"Index value {range} is too low.")
-        if range >= length:
-            raise IndexError(f"Index value {range} is too high")
+    if isinstance(range_obj, int):
+        if range_obj < -length:
+            raise IndexError(f"Index value {range_obj} is too low.")
+        if range_obj >= length:
+            raise IndexError(f"Index value {range_obj} is too high")
         idx_list = [
-            range % length
+            range_obj % length
         ]
 
-    elif isinstance(range, slice):
+    elif isinstance(range_obj, slice):
         idx_list = [
             i % length
-            for i in range(length)[range]
+            for i in range(length)[range_obj]
         ]
 
-    elif isinstance(range, list):
+    elif isinstance(range_obj, list):
         idx_list = [
-            idx % length for idx in range
+            idx % length for idx in range_obj
             if -length <= idx < length
         ]
 
     else:
-        raise ValueError(f"Invalid range value {range}")
+        raise ValueError(f"Invalid range value {range_obj}")
 
     return idx_list
