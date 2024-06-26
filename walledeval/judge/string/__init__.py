2cfcd8cfe754d671de0371cc03d17c31d102cda8
Prannaya <prannayagupta@gmail.com>
2024-06-26 21:54:56 +0800

    fix(judge): reimplement `StringMatchingJudge` to make it more "pythonic"

--- walledeval/judge/string/__init__.py
+++ walledeval/judge/string/__init__.py
@@ -1,42 +1,48 @@
 # walledeval/judge/string/__init__.py
-from pathlib import Path
 
 import yaml
+from pathlib import Path
 
-from walledeval.judge import Judge
+from walledeval.judge.core import Judge
 
-__all__ = ["StringMatchingJudge"]
+__all__ = [
+    "StringMatchingJudge"
+]
 
 
 class StringMatchingJudge(Judge[None, bool]):
-
-    def __init__(self, name: str, must_match_all: list[str] = None, must_mismatch_all: list[str] = None,
+    def __init__(self, name: str,
+                 must_match_all: list[str] = [],
+                 must_mismatch_all: list[str] = [],
                  caseless: bool = False):
         super().__init__(name)
-
-        self.must_match_all, self.must_mismatch_all, self.caseless = StringMatchingJudge.from_preset(name) if (
-                must_match_all is None and must_mismatch_all is None
-        ) else must_match_all, must_mismatch_all, caseless
-
-    def check(self, response: str, answer: None = None) -> bool:
-        text: str = response.casefold() if self.caseless else response
-
-        return (
-                (
-                        not self.must_mismatch_all
-                        or not any(target in text for target in self.must_mismatch_all)
-                )
-                and (
-                        not self.must_match_all
-                        or all(target in text for target in self.must_match_all)
-                )
+        
+        self.must_match_all = must_match_all
+        self.must_mismatch_all = must_mismatch_all
+        self.caseless = caseless
+    
+    @classmethod
+    def from_preset(cls, name: str = "beta"):
+        # presets are adapted from https://github.com/ThuCCSLab/JailbreakEval
+        yaml_fp = Path(__file__).resolve().parent / f"presets/{name}.yaml"
+        yaml_text = yaml_fp.read_text(encoding="utf-8")
+        yaml_dict: dict = yaml.safe_load(yaml_text)
+        
+        return cls(
+            must_match_all=yaml_dict.get("must_match_all", []),
+            must_mismatch_all=yaml_dict.get("must_mismatch_all", []),
+            caseless=yaml_dict.get("caseless", False)
         )
 
-    @classmethod
-    def from_preset(cls, name: str):
-        # presets are adapted from https://github.com/ThuCCSLab/JailbreakEval
+    def check(self, response: str, answer: None = None) -> bool:
+        text = response.casefold() if self.caseless else response
 
-        yaml_file_path = Path(f"/presets/{name}.yaml").resolve()
-        yaml_dict = yaml.safe_load(yaml_file_path.read_text(encoding="utf-8"))
-
-        return yaml_dict["must_match_all"], yaml_dict["must_mismatch_all"], yaml_dict["caseless"]
+        return (
+            (
+                not self.must_mismatch_all or 
+                not any(target in text for target in self.must_mismatch_all)
+            ) and (
+                not self.must_match_all or 
+                all(target in text for target in self.must_match_all)
+            )
+        )
