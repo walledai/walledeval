24c5df9df3fba36fbe8896b1dd50baf20ee9523c
Prannaya <prannayagupta@gmail.com>
2024-07-07 22:48:14 +0800

    fix: delete unnecessary code

--- walledeval/__init__.py
+++ walledeval/__init__.py
@@ -1,14 +1,1 @@
 # walledeval/__init__.py
-
-
-
-# from walledeval.llm import LLM, HF_LLM, hf_models, Claude
-# from walledeval.judge import Judge, ClaudeJudge
-# from walledeval.data import TestCase, Log, Benchmark, WMDP
-
-
-# __all__ = [
-#     "LLM", "HF_LLM", "hf_models", "Claude",
-#     "Judge", "ClaudeJudge",
-#     "TestCase", "Log", "Benchmark", "WMDP"
-# ]
