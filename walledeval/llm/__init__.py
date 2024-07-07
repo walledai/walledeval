986a4f7f11217167831e355b50ec34e5a0e7f8e1
Prannaya <prannayagupta@gmail.com>
2024-07-07 22:45:37 +0800

    patch(llm): Add `Gemini` to import list

--- walledeval/llm/__init__.py
+++ walledeval/llm/__init__.py
@@ -4,9 +4,11 @@ from walledeval.llm.core import LLM
 from walledeval.llm.huggingface import hf_models, HF_LLM
 from walledeval.llm.claude import Claude
 from walledeval.llm.openai import OpenAI
+from walledeval.llm.gemini import Gemini
 
 __all__ = [
     "LLM",
     "hf_models", "HF_LLM",
-    "Claude", "OpenAI"
+    "Claude", "OpenAI",
+    "Gemini"
 ]
