295947557d94b48c895ba3fcaebf2ac622e67e00
Prannaya <prannayagupta@gmail.com>
2024-07-08 11:55:25 +0800

    feat(llm): Implement `Llama` class for GGUF loading

---
+++ walledeval/llm/llama.py
@@ -0,0 +1,89 @@
+# walledeval/llm/llama.py
+
+import llama_cpp
+from typing import Optional, Union
+
+from walledeval.types import Message, Messages, LLMType
+from walledeval.llm.core import LLM
+
+
+
+class Llama(LLM):
+    def __init__(self,
+                 name: str,
+                 model: llama_cpp.Llama,
+                 system_prompt: str = "",
+                 type: Optional[Union[LLMType, int]] = LLMType.NEITHER,
+                 **kwargs):
+        super().__init__(name, system_prompt, type)
+        self.model = model
+    
+    @classmethod
+    def from_pretrained(cls,
+                        repo_id: str,
+                        filename: str,
+                        system_prompt: str = "",
+                        type: Optional[Union[LLMType, int]] = LLMType.NEITHER,
+                        **kwargs):
+        llm = llama_cpp.Llama.from_pretrained(
+            repo_id=repo_id,
+            filename=filename,
+            **kwargs
+        )
+        
+        return cls(
+            name = f"{repo_id}/{filename}",
+            model = llm,
+            system_prompt = system_prompt,
+            type = type
+        )
+    
+    def chat(self,
+             text: Messages,
+             max_new_tokens: int = 512,
+             temperature: float = 0.1) -> str:
+        messages: list[dict[str, str]]
+        if isinstance(text, str):
+            messages = [{
+                "role": "user",
+                "content": text
+            }]
+        elif isinstance(text, list) and isinstance(text[0], Message):
+            messages = [
+                dict(msg)
+                for msg in text
+            ]
+        elif isinstance(text, list) and isinstance(text[0], dict):
+            messages = text
+        else:
+            raise TypeError("Unsupported format for parameter 'text'")
+
+        if messages[0]["role"] != "system":
+            messages.insert(0, {
+                "role": "system",
+                "content": self.system_prompt
+            })
+        
+        
+        message = self.model.create_chat_completion(
+            messages,
+            max_tokens=max_new_tokens,
+            temperature=temperature
+        )
+        output = message.choices[0].message.content
+        return output
+    
+    def complete(self,
+                 text: str,
+                 max_new_tokens: int = 512,
+                 temperature: float = 0.1) -> str:
+        message = self.model(
+            text,
+            max_tokens=max_new_tokens,
+            temperature=temperature,
+            echo=False
+        )
+        output = message.choices[0].text
+        return output
+        
+
