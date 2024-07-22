a031cd074b031a3b1c16805b1ab79fe6f2dc481e
Prannaya <prannayagupta@gmail.com>
2024-07-22 21:08:16 +0800

    feat(attacks): implement attacks classes

--- walledeval/attacks/__init__.py
+++ walledeval/attacks/__init__.py
@@ -1,1 +1,11 @@
-# nothing yet 
+# walledeval/attacks/__init__.py
+
+from walledeval.attacks.core import (
+    Attack, UniversalAttack, PromptAttack,
+    MutatorAttack, CompositeAttack
+)
+
+__all__ = [
+    "Attack", "UniversalAttack", "PromptAttack",
+    "MutatorAttack", "CompositeAttack"
+]
