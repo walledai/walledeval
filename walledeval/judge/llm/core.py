2c113e6f689dcfb54dabc24e6d7c07a37fbed2b7
Prannaya <prannayagupta@gmail.com>
2024-07-22 22:36:38 +0800

    fix(llmasajudge): remove the changes from @nlccesc

--- walledeval/judge/llm/core.py
+++ walledeval/judge/llm/core.py
@@ -5,33 +5,29 @@ from typing import Generic, TypeVar
 
 from walledeval.llm import LLM
 from walledeval.judge.core import Judge
-from walledeval.prompts import PromptTemplate
 
 __all__ = [
     "LLMasaJudge"
 ]
 
-O = TypeVar('O')  # Output Field
-S = TypeVar('S')  # Score Field
+O = TypeVar('O') # Output Field
+S = TypeVar('S') # Score Field
 
 
 class LLMasaJudge(Judge[None, O, S], ABC, Generic[O, S]):
     def __init__(self,
                  name: str,
-                 llm: LLM,
-                 prompt_template: PromptTemplate):
+                 llm: LLM):
         super().__init__(name)
         self._llm = llm
-        self._prompt_template = prompt_template
-
+    
     def set_system_prompt(self,
                           system_prompt: str):
         self._llm = self._llm.set_system_prompt(system_prompt)
-
-    def generate(self, prompt: str, **kwargs) -> str:
-        formatted_prompt = self._prompt_template.format(prompt=prompt, **kwargs)
+        
+    def generate(self, response: str) -> str:
         return self._llm.generate(
-            formatted_prompt,
+            response,
             instruct=True
         )
 
@@ -41,5 +37,7 @@ class LLMasaJudge(Judge[None, O, S], ABC, Generic[O, S]):
 
     def check(self, response: str, answer: None = None) -> O:
         llm_output = self.generate(response)
+
         output = self.process_llm_output(llm_output)
+
         return output
