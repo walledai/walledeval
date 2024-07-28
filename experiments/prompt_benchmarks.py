65bf7438b05dd7ccbc0656e3c34859dc37f4c3b0
Prannaya <prannayagupta@gmail.com>
2024-07-28 21:43:14 +0800

    feat(experiments): improve benchmark script in experiments

--- experiments/prompt_benchmarks.py
+++ experiments/prompt_benchmarks.py
@@ -14,12 +14,19 @@ import argparse
 
 import pathlib
 
+sys.path.append(str(pathlib.Path(__file__).resolve().parent.parent))
+
+from walledeval.data import HuggingFaceDataset
+from walledeval.prompts import PromptTemplate
+from walledeval.llm import HF_LLM
+from walledeval.judge import LlamaGuardJudge
+
+
 dataset_args = {
     "harmbench": ("walledai/HarmBench", "standard"),
-    "advbench": ("walledai/AdvBench"),
+    "advbench": ("walledai/AdvBench", ),
     "aya-redteaming": ("walledai/AyaRedTeaming", "default", "english"),
-    "xstest": ("walledai/XSTest"),
-    
+    #"xstest": ("walledai/XSTest"),   
 }
 
 
@@ -45,38 +52,47 @@ if __name__ == "__main__":
     
     parser.add_argument("-t", "--token_name", default = "HF_TOKEN", help="Environment Variable for token")
     
+    parser.add_argument("-v", "--verbose", help="Print running logs", action="store_true")
+    
+    parser.add_argument("-i", "--interval", type=int, default=100, help="Number of runs before saving")
+    
+    parser.add_argument("-n", "--num", type=int, default=0, help="Number of samples to test")
+    
     
     args = parser.parse_args()
     
     llm_name = args.model
     dataset_name = args.dataset
+    
     output_filename = args.filename if args.filename else f"experiments/logs/{dataset_name}/{llm_name}.json"
+    os.makedirs(
+        os.path.dirname(output_filename),
+        exist_ok=True
+    )
+    
+    verbose = bool(args.verbose)
+    interval = args.interval
+    
+    num = args.num
     
     load_dotenv(args.env)
     if token := os.getenv(args.token_name):
         login(token)
-        
-    sys.path.append(str(pathlib.Path(__file__).resolve().parent.parent))
     
-    from walledeval.data import HuggingFaceDataset
-    from walledeval.prompts import PromptTemplate
-    from walledeval.llm import HF_LLM
-    from walledeval.judge import LlamaGuardJudge
-
-
+    
+    # ==================================================
+    # ============== STEP 1: LOAD DATASET ==============
+    # ==================================================
+    
     dataset = HuggingFaceDataset.from_hub(*(dataset_args[dataset_name]))
     template = PromptTemplate()
 
-    # if dataset_name == "HarmBench":
-    #     # only the 200 rows
-    #     dataset = HuggingFaceDataset.from_hub("walledai/HarmBench", "standard")
-    #     template = PromptTemplate()
-    # elif dataset_name == "AdvBench":
-    #     dataset = HuggingFaceDataset.from_hub("walledai/AdvBench")
-    #     template = PromptTemplate()
+    samples = dataset.all() if num == 0 else dataset.sample(num)
 
-    samples = dataset.all()
 
+    # ====================================================
+    # ============== STEP 2: LOAD LLM MODEL ==============
+    # ====================================================
 
     sut_kwargs = dict(
         type = 1,
@@ -88,21 +104,19 @@ if __name__ == "__main__":
         "quantization_config": {"load_in_4bit": True}
     }
 
+    # Llama Models
     if llm_name == "llama3.1-8b":
         sut = HF_LLM("unsloth/Meta-Llama-3.1-8B-Instruct-bnb-4bit", **sut_kwargs)
     elif llm_name == "llama3-8b":
         sut = HF_LLM("unsloth/llama-3-8b-Instruct-bnb-4bit", **sut_kwargs)
     elif llm_name == "llama2-7b": # no unsloth model
-        sut = HF_LLM("meta-llama/Llama-2-7b-chat-hf", **sut_kwargs, 
-                     model_kwargs = {**model_kwargs, **(sut_kwargs["model_kwargs"])})
-    # elif llm_name == "llama2-13b": # no unsloth model
-    #     sut = HF_LLM("meta-llama/Llama-2-13b-chat-hf", **sut_kwargs, 
-    #                  model_kwargs = {**model_kwargs, **(sut_kwargs["model_kwargs"])})
+        sut = HF_LLM("meta-llama/Llama-2-7b-chat-hf", **sut_kwargs)
     
+    # Gemma Models
     elif llm_name == "gemma2-9b":
         sut = HF_LLM("unsloth/gemma-2-9b-it-bnb-4bit", **sut_kwargs)
-        
-        
+    
+    # Mistral Models
     elif llm_name == "mistral-nemo-12b":
         sut = HF_LLM("unsloth/Mistral-Nemo-Instruct-2407-bnb-4bit", **sut_kwargs)
     elif llm_name == "mistral-7b":
@@ -110,43 +124,75 @@ if __name__ == "__main__":
     elif llm_name == "mixtral-8x7b":
         sut = HF_LLM("ybelkada/Mixtral-8x7B-Instruct-v0.1-bnb-4bit", **sut_kwargs)
         
-        
-        
+    # Phi Models
     elif llm_name == "phi3-small": # no unsloth model
         sut = None
-        
-        
-        
+        raise NotImplementedError
+    
+    # Qwen Models
     elif llm_name == "qwen2-7b": # no unsloth model
         sut = HF_LLM("unsloth/Qwen2-7B-Instruct-bnb-4bit", **sut_kwargs)
     elif llm_name == "qwen2-1.5b": # no unsloth model
         sut = HF_LLM("unsloth/Qwen2-1.5B-Instruct-bnb-4bit", **sut_kwargs)
     elif llm_name == "qwen2-0.5b": # no unsloth model
         sut = HF_LLM("unsloth/Qwen2-0.5B-Instruct-bnb-4bit", **sut_kwargs)
-        
+    
+
+    # =====================================================
+    # ============== STEP 3: LOAD LLAMAGUARD ==============
+    # =====================================================
+    
     judge = LlamaGuardJudge(2, model_kwargs=model_kwargs, device_map="auto")
 
 
+    # ================================================================
+    # ============== STEP 3: TEST MODEL AGAINST DATASET ==============
+    # ================================================================
+    
+    running_score = 0
     logs = []
 
-    for i, sample in tqdm(enumerate(samples)):
-        prompt = template(sample)
-        response = sut(prompt)
+    try:
+        for i, sample in tqdm(enumerate(samples)):
+            prompt = template(sample)
+            response = sut(prompt)
 
-        if dataset_name == "XSTest":
-            judge_output, score = judge(response, int(sample.label == "safe"))
-        else:
             judge_output, score = judge(response)
-        logs.append({
-            "sample": sample.prompt, # all are by default prompt datasets
-            "prompt": prompt,
-            "response": response,
-            "judge_output": judge_output,
-            "score": score # True if safe, False if unsafe
-        })
-    
-    with open(output_filename, "w+") as f:
-        json.dump(logs, f)
+            
+            logs.append({
+                "sample": sample.prompt, # all are by default prompt datasets
+                "prompt": prompt,
+                "response": response,
+                "judge_output": judge_output,
+                "score": score # True if safe, False if unsafe
+            })
+            
+            if score:
+                running_score += 1
+            
+            if verbose:
+                print("\n\n---------")
+                print(f"{i+1}/{len(samples)}")
+                print("sample:", prompt)
+                print("response:", response)
+                print("judge output:", judge_output._value_)
+                print("score:", score)
+                print("running score:", round(running_score / (i+1), 3))
 
-
-
+            if (i+1) % interval == 0:
+                with open(output_filename, "w") as f:
+                    json.dump(logs, f, indent=4)
+                
+                if verbose:
+                    print("Saved", i+1, "logs to", output_filename)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        if verbose:
+            print("\n\n\n---------")
+            print("Final score:", round(running_score/len(logs), 3))
+        
+        with open(output_filename, "w+") as f:
+            json.dump(logs, f, indent=4)
+            if verbose:
+                print("Saved", i+1, "logs to", output_filename)
