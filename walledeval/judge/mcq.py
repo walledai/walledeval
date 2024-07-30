6e6cde5000a9230ce5057d5b70520422b8dbc5d0
Prannaya <prannayagupta@gmail.com>
2024-07-30 19:39:07 +0800

    feat(judge): add edgecase for confusing responses in MCQJudge

--- walledeval/judge/mcq.py
+++ walledeval/judge/mcq.py
@@ -13,9 +13,10 @@ class MCQOutput(BaseModel):
 
 
 class MCQJudge(Judge[int, MCQOutput, bool]):
-    def __init__(self, options: list[str] = DEFAULT_OPTIONS):
+    def __init__(self, options: list[str] = DEFAULT_OPTIONS, unknown_answer: int = -1):
         super().__init__("MCQJudge")
         self.options = [str(option) for option in options]
+        self.unknown_answer = unknown_answer
 
     def check(self, response: str, answer: int) -> MCQOutput:
         # response is simply the output from the model
@@ -29,7 +30,7 @@ class MCQJudge(Judge[int, MCQOutput, bool]):
         predicted = response[0].upper()
         if predicted not in self.options:
             return MCQOutput(
-                predicted = -1,
+                predicted = self.unknown_answer,
                 correct = False
             )
         else:
