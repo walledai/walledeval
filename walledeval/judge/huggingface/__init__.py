26c7df3d78d1e59b380b584f0b5078249c2f3a39
Prannaya <prannayagupta@gmail.com>
2024-07-31 10:02:51 +0800

    feat(judge): cleanup huggingface judge code

--- walledeval/judge/huggingface/__init__.py
+++ walledeval/judge/huggingface/__init__.py
@@ -1,2 +1,13 @@
+# walledeval/judge/huggingface/__init__.py
+
 from walledeval.judge.huggingface.core import HFTextClassificationJudge
-from walledeval.judge.huggingface.models import GPTFuzzJudge, UnitaryJudge, RobertaToxicityJudge
+from walledeval.judge.huggingface.models import (
+    GPTFuzzJudge, UnitaryJudge,
+    RobertaToxicityJudge
+)
+
+__all__ = [
+    "HFTextClassificationJudge",
+    "GPTFuzzJudge", "UnitaryJudge",
+    "RobertaToxicityJudge"
+]
