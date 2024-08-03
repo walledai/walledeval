c4ef75de17ce966be07b8c19eb2b207a4c1c6dde
Prannaya <prannayagupta@gmail.com>
2024-08-03 12:48:59 +0800

    fix(experiments): modify runs directory

--- experiments/scripts/run_mutations.sh
+++ experiments/scripts/run_mutations.sh
@@ -1,13 +1,13 @@
 echo "$(pwd)"
 
 echo "Starting Generation on HarmBench with Mistral"
-python experiments/mutation_generation.py -d harmbench -m mistral-7b -v > runs/mutations-harmbench-mistral.log 2> runs/mutations-harmbench-mistral.log
+python experiments/mutation_generation.py -d harmbench -m mistral-7b -v > experiments/runs/mutations-harmbench-mistral.log 2> experiments/runs/mutations-harmbench-mistral.log
 echo "Starting Generation on HarmBench with Llama 2"
-python experiments/mutation_generation.py -d harmbench -m llama2-7b-uncensored -v > runs/mutations-harmbench-llama2.log 2> runs/mutations-harmbench-llama2.log
+python experiments/mutation_generation.py -d harmbench -m llama2-7b-uncensored -v > experiments/runs/mutations-harmbench-llama2.log 2> experiments/runs/mutations-harmbench-llama2.log
 
 echo "Starting Generation on XSTest with Mistral"
-python experiments/mutation_generation.py -d xstest -m mistral-7b -v > runs/mutations-xstest-mistral.log 2> runs/mutations-xstest-mistral.log
+python experiments/mutation_generation.py -d xstest -m mistral-7b -v > experiments/runs/mutations-xstest-mistral.log 2> experiments/runs/mutations-xstest-mistral.log
 echo "Starting Generation on XSTest with Llama 2"
-python experiments/mutation_generation.py -d xstest -m llama2-7b-uncensored -v > runs/mutations-xstest-llama2.log 2> runs/mutations-xstest-llama2.log
+python experiments/mutation_generation.py -d xstest -m llama2-7b-uncensored -v > experiments/runs/mutations-xstest-llama2.log 2> experiments/runs/mutations-xstest-llama2.log
 
 echo "Finished Generation"
